5542250e10668156403bbdf14a9f70c33211a8de
claudiaklo <clauding398@gmail.com>
2026-06-01 05:50:41 +0100

    retour en arrière

--- src/app/_routes/layout.tsx
+++ src/app/_routes/layout.tsx
@@ -295,8 +295,6 @@ const RootLayout: LayoutComponent = () => {
       `}</style>
 
       {/* ===== CONTENU ===== */}
-      {/* Spacer pour compenser la navbar fixed de 60px */}
-      <div style={{ height: '60px', flexShrink: 0 }} aria-hidden="true" />
       <main style={{ flex: 1 }}>
         <Outlet />
       </main>
